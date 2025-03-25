e6465eaa037c4910ba0176f0ed76334f4fb61e1f
Letícia <122921732+LeticiaVicenteTeodoro@users.noreply.github.com>
2025-03-25 19:59:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const db = require("./database");
 
+// Teste de consulta
+db.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.error("Erro ao executar consulta:", err);
+  } else {
+    console.log("Hora atual no banco de dados:", res.rows[0]);
+  }
+  db.end(); // Fecha a conexão
+});
